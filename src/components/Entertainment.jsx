fc2da49b81b4904c674bd4df4b5bfad668bde6cc
ishantsingh <singhishant37@gmail.com>
2025-05-01 15:09:13 +0530

    Deleted

--- src/components/Entertainment.jsx
+++ src/components/Entertainment.jsx
@@ -4,11 +4,7 @@ import News from './News';
 function Entertainment() {
   return (
     <div>
-<<<<<<< HEAD
       {/* <h2 className="text-center mt-5">Entertainment News</h2> */}
-=======
-{/*       <h2 className="text-center mt-5">Entertainment News</h2> */}
->>>>>>> 66f55b78be2e240cd138408610eb7ad2afc6f201
       <News category="entertainment" />
     </div>
   );
--- src/components/Sports.jsx
+++ src/components/Sports.jsx
@@ -4,14 +4,10 @@ import News from './News';
 function Sports() {
   return (
     <div>
-<<<<<<< HEAD
-      {/* <h2 className="text-center mt-5">Entertainment News</h2> */}
-      {/* new */}
-      <News category="Sports" />
-=======
+      {/* <h2 className="text-center mt-5">Sports News</h2> */}
 {/*       <h2 className="text-center mt-5">Sports News</h2> */}
       <News category="sports" />
->>>>>>> 66f55b78be2e240cd138408610eb7ad2afc6f201
+
     </div>
   );
 }

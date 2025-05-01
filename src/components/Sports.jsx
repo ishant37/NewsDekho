f238dd6389078774bb3d5f104f94398eeb27498b
ishantsingh <singhishant37@gmail.com>
2025-05-01 14:48:53 +0530

    update sports

--- src/components/Sports.jsx
+++ src/components/Sports.jsx
@@ -4,8 +4,9 @@ import News from './News';
 function Sports() {
   return (
     <div>
-      {/* <h2 className="text-center mt-5">Sports News</h2> */}
-      <News category="sports" />
+      {/* <h2 className="text-center mt-5">Entertainment News</h2> */}
+      {/* new */}
+      <News category="Sports" />
     </div>
   );
 }
